36c7e0d120aea1832d2c70a87b023d2811bbfc24
Moses <geremoses1@gmail.com>
2022-09-12 10:38:41 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ *  Description: prints "\"Programming is like building a multilingual puzzle"
+ */
+int main(void)
+{
+	prinf("\nProgramming is like building a multilingual puzzle");
+	return (0);
+}
